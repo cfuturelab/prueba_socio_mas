606788a8594828167e91f56abb5f19e365fd20d1
cfuturelab <115724634+cfuturelab@users.noreply.github.com>
2021-03-21 18:40:12 +0000

    Développeur Back-end en startup FINTECH (H/F) en Stage à Paris

--- blockChain.py
+++ blockChain.py
@@ -10,4 +10,4 @@ class Block:
     def __init__(self, data, previousHash):
         self.data = data
         self.previousHash = previousHash
-        self.hash = sel
+        self.hash = self.Compu

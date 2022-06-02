138aef6965383f9f116d4715dd37071b1b44e86c
cfuturelab <115724634+cfuturelab@users.noreply.github.com>
2022-06-02 07:04:36 +0000

    add list command

--- blockChain.py
+++ blockChain.py
@@ -52,4 +52,4 @@ class Chain:
         for i in range(1,len(self.chain)):
             blockToValidate = self.chain[i]
             if (blockToValidate.hash) != blockToValidate.ComputeHash:
-          
+             
